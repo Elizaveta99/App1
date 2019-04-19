524fb936e8780a5db74c98ef6f11962454e52034
eliz <eliz.vitebsk@gmail.com>
2019-04-19 14:51:02 -0700

    Initial Commit

---
+++ lab6_task3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lab6_task3
+//
+//  Created by Lizaveta Rudzko on 1/30/1398 AP.
+//  Copyright © 1398 Lizaveta Rudzko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
